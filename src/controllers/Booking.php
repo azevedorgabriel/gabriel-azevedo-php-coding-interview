e45be63dd9af1dbf35b115531ce727028e2eed7e
Gabriel Azevedo <azevedorgabriel@gmail.com>
2024-07-01 15:10:39 -0300

    challenge-interview

--- src/controllers/Booking.php
+++ src/controllers/Booking.php
@@ -3,14 +3,61 @@
 namespace Src\controllers;
 
 use Src\models\BookingModel;
+use Src\models\DogModel;
 
 class Booking {
+
+	private $dogs;
 
 	private function getBookingModel(): BookingModel {
 		return new BookingModel();
 	}
 
+	private function getDogModel(): DogModel {
+		return new DogModel();
+	}
+
 	public function getBookings() {
 		return $this->getBookingModel()->getBookings();
+	}
+
+	/**
+	 * Description: To book a new client (considering client id, price, check-in date and check-out date)
+	 * @param int $clientId
+	 * @param int $price
+	 * @param string $checkIn
+	 * @param string $checkOut
+	 * @return array
+	 */
+	public function createBooking($clientId, $price, $checkIn, $checkOut) {
+		//if client has dogs and check average age of dogs
+		if ($this->clientHasDogs($clientId) && $this->clientDeserveDiscount()) {
+			//giving 10% of discount
+			$price = ($price * 0.9);
+		}
+
+		return $this->getBookingModel()->createBooking($clientId, $price, $checkIn, $checkOut);
+	}
+
+	private function clientHasDogs($clientId) {
+		$this->dogs = $this->getDogModel()->getDogsByClientId($clientId);
+
+		return count($this->dogs) > 0;
+	}
+
+	/**
+	 * Description: To give a 10% discount in the booking if the average age of dogs of a client registered is less than 10 years
+	 * @return boolean
+	 */
+	private function clientDeserveDiscount() {
+		$sumAgeDogs = 0;
+
+		foreach ($this->dogs as $dog) {
+			$sumAgeDogs += $dog['age'];
+		}
+
+		$average = ($sumAgeDogs / count($this->dogs));
+
+		return $average < 10;
 	}
 }
--- tests/BookingTest.php
+++ tests/BookingTest.php
@@ -31,4 +31,59 @@ class BookingTest extends TestCase {
 		$this->assertEquals($results[0]['checkindate'], '2021-08-04 15:00:00');
 		$this->assertEquals($results[0]['checkoutdate'], '2021-08-11 15:00:00');
 	}
+
+	/** @test */
+	public function createBooking() {
+		$clientId = 1;
+		$price = 500;
+		$checkIn = '2024-07-01 15:00:00';
+		$checkOut = '2024-07-05 10:00:00';
+
+		$this->booking->createBooking($clientId, $price, $checkIn, $checkOut);
+		$results = $this->booking->getBookings();
+
+		$this->assertIsArray($results);
+	}
+
+	/** @test */
+	public function createBookingWithDiscount() {
+		$clientId = 1;
+		$price = 500;
+		$checkIn = '2024-07-01 15:00:00';
+		$checkOut = '2024-07-05 10:00:00';
+
+		$booking = $this->booking->createBooking($clientId, $price, $checkIn, $checkOut);
+		$results = $this->booking->getBookings();
+
+		$this->assertIsArray($results);
+
+		$bookingWithDiscount = end($results);
+
+		$this->assertEquals($bookingWithDiscount['id'], $booking['id']);
+		$this->assertEquals($bookingWithDiscount['clientid'], 1);
+		$this->assertEquals($bookingWithDiscount['price'], 450);
+		$this->assertEquals($bookingWithDiscount['checkindate'], '2024-07-01 15:00:00');
+		$this->assertEquals($bookingWithDiscount['checkoutdate'], '2024-07-05 10:00:00');
+	}
+
+	/** @test */
+	public function createBookingWithoutDiscount() {
+		$clientId = 3;
+		$price = 500;
+		$checkIn = '2024-07-01 15:00:00';
+		$checkOut = '2024-07-05 10:00:00';
+
+		$booking = $this->booking->createBooking($clientId, $price, $checkIn, $checkOut);
+		$results = $this->booking->getBookings();
+
+		$this->assertIsArray($results);
+
+		$bookingWithoutDiscount = end($results);
+
+		$this->assertEquals($bookingWithoutDiscount['id'], $booking['id']);
+		$this->assertEquals($bookingWithoutDiscount['clientid'], 3);
+		$this->assertEquals($bookingWithoutDiscount['price'], 500);
+		$this->assertEquals($bookingWithoutDiscount['checkindate'], '2024-07-01 15:00:00');
+		$this->assertEquals($bookingWithoutDiscount['checkoutdate'], '2024-07-05 10:00:00');
+	}
 }
